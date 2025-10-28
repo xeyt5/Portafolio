18b20eb4796a923413cd9ef559be4f556663201f
Xeyt <cbwyuren500@gmail.com>
2025-10-28 14:56:57 -0600

    Add frontend and backend with complete project structure

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'inicio',
+        loadComponent: () => import('./modules/user/dashboard/dashboard').then(m => m.Dashboard)
+    }
+];
