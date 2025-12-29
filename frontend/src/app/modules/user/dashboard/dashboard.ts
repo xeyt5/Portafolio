156a720dfac7fc57b5f866cc52571c2d092fb54b
Xeyt <cbwyuren500@gmail.com>
2025-12-29 13:39:52 -0600

    [UPDATE]: nuevas secciones para mis videos en el dashboar, creacion del modulo pipes para la obtencion de urls y cambio de nombre y logo

--- frontend/src/app/modules/user/dashboard/dashboard.ts
+++ frontend/src/app/modules/user/dashboard/dashboard.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import  { RouterLink } from '@angular/router';
+import { SafeUrlPipe } from '../../../pipes/safe-url-pipe';
+import { title } from 'process';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, SafeUrlPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
@@ -14,6 +16,25 @@ export class Dashboard {
     name:"xeyt",
     avatar: "https://i.pravatar.cc/150?img=2" 
   }
+  videos = [
+    {
+      title: '¡Las 10 apps que no pueden faltar en tu Linux!',
+      description: '¡Hola a todos! Bienvenidos a mi canal. Hoy les traigo un video súper interesante sobre 10 apps para linux!!!',
+      url: 'https://www.youtube.com/embed/kEsILbeD13g'
+    },
+    {
+      title: 'Lo que Windows NO te permite hacer (pero Linux sí)',
+      description: 'Hola a todos! Bienvenidos a mi canal. Hoy les traigo un video súper interesante donde te muestro cosas que Windows no te permite hacer pero Linux sí!!!',
+      url: 'https://www.youtube.com/embed/EaMvGe8Nqzs'
+      
+    },
+    {
+      title: '¡Mira cómo Hyperland cambia tu Linux!',
+      description: '¡Hola a todos! Bienvenidos a mi canal. Hoy les traigo un video súper interesante sobre Hyperland, un gestor de ventanas para Linux que transformará tu experiencia visual!!!',
+      url: 'https://www.youtube.com/embed/oC4aE2Qc5tE'
+    }
+
+  ];
   
 
 }
