f12df761e8ca1aeb50e5cda2c843c997f126b835
Xeyt <cbwyuren500@gmail.com>
2025-12-30 14:39:14 -0600

    [UPDATE]: nuevo modulo proyectos, creacion de 3 proyectos + estilo

--- frontend/src/app/modules/user/proyectos/proyectos.ts
+++ frontend/src/app/modules/user/proyectos/proyectos.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './proyectos.css'
 })
 export class Proyectos {
+
+
+  projects = [
+    {
+      title: 'Nombre 1',
+      description: 'Descripción del proyecto 1',
+      link: 'https://example.com/proyecto1'
+    },
+    {
+      title: 'Nombre 2',
+      description: 'Descripción del proyecto 2',
+      link: 'https://example.com/proyecto2'
+    },
+    {
+      title: 'Nombre 3',
+      description: 'Descripción del proyecto 3',
+      link: 'https://example.com/proyecto3'
+    }
+  ]
 
 }
