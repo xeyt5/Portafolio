4e37d9dff86b37c2d7898f69138d51e149fee79a
Xeyt <cbwyuren500@gmail.com>
2026-01-07 13:08:04 -0600

    [UPDATE] se agrego proyectos y estilos en el modulo de proyectos

--- frontend/src/app/modules/user/proyectos/proyectos.ts
+++ frontend/src/app/modules/user/proyectos/proyectos.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-proyectos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './proyectos.html',
   styleUrl: './proyectos.css'
 })
@@ -14,17 +14,39 @@ export class Proyectos {
     {
       title: 'Nombre 1',
       description: 'Descripción del proyecto 1',
-      link: 'https://example.com/proyecto1'
+      link: 'https://example.com/proyecto1',
+      img: 'cargando.avif'
+
     },
     {
       title: 'Nombre 2',
       description: 'Descripción del proyecto 2',
-      link: 'https://example.com/proyecto2'
+      link: 'https://example.com/proyecto2',
+      img: 'cargando.avif'
     },
     {
       title: 'Nombre 3',
       description: 'Descripción del proyecto 3',
-      link: 'https://example.com/proyecto3'
+      link: 'https://example.com/proyecto3',
+      img: 'cargando.avif'
+    },
+    {
+      title: 'Nombre 4',
+      description: 'Descripción del proyecto 3',
+      link: 'https://example.com/proyecto3',
+      img: 'cargando.avif'
+    },
+    {
+      title: 'Nombre 5',
+      description: 'Descripción del proyecto 3',
+      link: 'https://example.com/proyecto3',
+      img: 'cargando.avif'
+    },
+    {
+      title: 'Nombre 6',
+      description: 'Descripción del proyecto 3',
+      link: 'https://example.com/proyecto3',
+      img: 'cargando.avif'
     }
   ]
 
